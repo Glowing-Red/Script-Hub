11ff863f3ef929a61f9a6c6bca882f27c09be61b
Glowing-Red <79102132+Glowing-Red@users.noreply.github.com>
2022-01-15 23:07:28 +0100

    Update Universals.lua

--- Storage/Universals.lua
+++ Storage/Universals.lua
@@ -1,27 +1,35 @@
 Universals = {
 	
-  Hydroxide = {
+	Hydroxide = {
 		Name = "Hydroxide",
 		SearchName = "Hydroxide",
-		Colour = Color3.fromRGB(141, 42, 22),
+		Icon = "Default",
+		IconColour = Color3.fromRGB(141, 42, 22),
+		BackgroundColour = Color3.fromRGB(141, 42, 22),
 		Code = "https://raw.githubusercontent.com/Glowing-Red/Script-Hub/main/Hydroxide.lua",
 	},
 	ServerBrowser = {
 		Name = "Server Browser",
 		SearchName = "Server Browser",
-		Colour = Color3.fromRGB(141, 42, 22),
+		Icon = "Default",
+		IconColour = Color3.fromRGB(141, 42, 22),
+		BackgroundColour = Color3.fromRGB(141, 42, 22),
 		Code = "https://raw.githubusercontent.com/Glowing-Red/Script-Hub/main/Server%20Browser.lua",
 	},
 	CmdX = {
 		Name = "CMD-X",
 		SearchName = "CMDX",
-		Colour = Color3.fromRGB(141, 42, 22),
+		Icon = "Default",
+		IconColour = Color3.fromRGB(141, 42, 22),
+		BackgroundColour = Color3.fromRGB(141, 42, 22),
 		Code = "https://raw.githubusercontent.com/CMD-X/CMD-X/master/Source",
 	},
 	InfiniteYield = {
 		Name = "Infinite Yield",
 		SearchName = "Infinite Yield",
-		Colour = Color3.fromRGB(141, 42, 22),
+		Icon = "Default",
+		IconColour = Color3.fromRGB(141, 42, 22),
+		BackgroundColour = Color3.fromRGB(141, 42, 22),
 		Code = "https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source",	
 	},
 }
