5370ca1997178a183bb41a892441a4e956ec7098
Glowing-Red <79102132+Glowing-Red@users.noreply.github.com>
2022-01-15 23:23:34 +0100

    Update Menus.lua

--- Storage/Menus.lua
+++ Storage/Menus.lua
@@ -3,7 +3,10 @@ Menus = {
 	BGTD = {
 		Name = "Bubble Gum Tower Defense",
 		SearchName = "Bubble Gum Tower Defense",
-		Colour = Color3.fromRGB(68, 131, 49),
+		Icon = "Default",
+		CustomIcon = "None",
+		CustomIconColour = Color3.fromRGB(141, 42, 22),
+		BackgroundColour = Color3.fromRGB(68, 131, 49),
 		Code = "https://raw.githubusercontent.com/Glowing-Red/Script-Hub/main/Bubble%20Gum%20Tower%20Defense.lua",
 	},
 }
