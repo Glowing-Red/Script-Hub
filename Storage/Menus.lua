2215b6ee35a87c3fd656bf3853c5392b5b50d068
Glowing-Red <79102132+Glowing-Red@users.noreply.github.com>
2022-01-15 23:54:54 +0100

    Update Menus.lua

--- Storage/Menus.lua
+++ Storage/Menus.lua
@@ -6,7 +6,7 @@ Menus = {
 		Icon = "Custom",
 		CustomIcon = "6948037100",
 		CustomIconColour = Color3.fromRGB(255, 242, 211),
-		CustomTextColour = Color3.fromRGB(255, 255, 255),
+		CustomTextColour = Color3.new(255, 255, 255),
 		BackgroundColour = Color3.fromRGB(68, 131, 49),
 		Code = "https://raw.githubusercontent.com/Glowing-Red/Script-Hub/main/Bubble%20Gum%20Tower%20Defense.lua",
 	},
