8b3cf6901ad51ff582d6deb3f568665a49b70bfb
Glowing-Red <79102132+Glowing-Red@users.noreply.github.com>
2022-01-15 22:47:03 +0100

    Update and rename Guis.lua to Menus.lua

---
+++ Storage/Menus.lua
@@ -0,0 +1,11 @@
+Menus = {
+	
+	BGTD = {
+		Name = "Bubble Gum Tower Defense",
+		SearchName = "Bubble Gum Tower Defense",
+		Colour = Color3.fromRGB(68, 131, 49),
+		Code = "https://raw.githubusercontent.com/Glowing-Red/Script-Hub/main/Bubble%20Gum%20Tower%20Defense.lua",
+	},
+}
+
+return Menus
