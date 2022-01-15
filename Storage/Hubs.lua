54986ead8f5b8de37e6e261fc606a82e70af5c9b
Glowing-Red <79102132+Glowing-Red@users.noreply.github.com>
2022-01-15 22:48:15 +0100

    Create Hubs.lua

---
+++ Storage/Hubs.lua
@@ -0,0 +1,11 @@
+Hubs = {
+	
+	EzHub = {
+		Name = "EZHub",
+		SearchName = "EzHub",
+		Colour = Color3.fromRGB(77, 102, 191),
+		Code = "https://raw.githubusercontent.com/debug420/Ez-Industries-Launcher-Data/master/Launcher.lua"
+	},
+}
+
+return Hubs
