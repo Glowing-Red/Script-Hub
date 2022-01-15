efcfee834d7910d8db3383f3cd5e64db42c813d7
Glowing-Red <79102132+Glowing-Red@users.noreply.github.com>
2022-01-15 23:31:21 +0100

    Update Universals.lua

--- Storage/Universals.lua
+++ Storage/Universals.lua
@@ -3,9 +3,9 @@ Universals = {
 	Hydroxide = {
 		Name = "Hydroxide",
 		SearchName = "Hydroxide",
-		Icon = "Custom",
-		CustomIcon = "6948037100",
-		CustomIconColour = Color3.fromRGB(255, 242, 211),
+		Icon = "Default",
+		CustomIcon = "None",
+		CustomIconColour = Color3.fromRGB(141, 42, 22),
 		BackgroundColour = Color3.fromRGB(141, 42, 22),
 		Code = "https://raw.githubusercontent.com/Glowing-Red/Script-Hub/main/Hydroxide.lua",
 	},
