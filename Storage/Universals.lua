940de0541f6b53d3ba7944a5cc1c930d9d3eead3
Glowing-Red <79102132+Glowing-Red@users.noreply.github.com>
2022-01-15 22:47:47 +0100

    Create Universals.lua

---
+++ Storage/Universals.lua
@@ -0,0 +1,29 @@
+Universals = {
+	
+  Hydroxide = {
+		Name = "Hydroxide",
+		SearchName = "Hydroxide",
+		Colour = Color3.fromRGB(141, 42, 22),
+		Code = "https://raw.githubusercontent.com/Glowing-Red/Script-Hub/main/Hydroxide.lua",
+	},
+	ServerBrowser = {
+		Name = "Server Browser",
+		SearchName = "Server Browser",
+		Colour = Color3.fromRGB(141, 42, 22),
+		Code = "https://raw.githubusercontent.com/Glowing-Red/Script-Hub/main/Server%20Browser.lua",
+	},
+	CmdX = {
+		Name = "CMD-X",
+		SearchName = "CMD-X",
+		Colour = Color3.fromRGB(141, 42, 22),
+		Code = "https://raw.githubusercontent.com/CMD-X/CMD-X/master/Source",
+	},
+	InfiniteYield = {
+		Name = "Infinite Yield",
+		SearchName = "Infinite Yield",
+		Colour = Color3.fromRGB(141, 42, 22),
+		Code = "https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source",	
+	},
+}
+
+return Universals
