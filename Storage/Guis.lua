2daf1ed97a5fb5da3b846e6fe1f605cbe4e0c98c
Glowing-Red <79102132+Glowing-Red@users.noreply.github.com>
2022-01-15 22:38:30 +0100

    Create Guis.lua

---
+++ Storage/Guis.lua
@@ -0,0 +1,47 @@
+ScriptsStorage = {
+	
+	Hydroxide = {
+		Name = "Hydroxide",
+    SearchName = "Hydroxide",
+		Author = "Universal",
+		Colour = Color3.fromRGB(141, 42, 22),
+		Code = "https://raw.githubusercontent.com/Glowing-Red/Script-Hub/main/Hydroxide.lua",
+	},
+	BGTD = {
+		Name = "Bubble Gum Tower Defense",
+    SearchName = "Bubble Gum Tower Defense",
+		Author = "Menu",
+		Colour = Color3.fromRGB(68, 131, 49),
+		Code = "https://raw.githubusercontent.com/Glowing-Red/Script-Hub/main/Bubble%20Gum%20Tower%20Defense.lua",
+	},
+	ServerBrowser = {
+		Name = "Server Browser",
+    SearchName = "Server Browser",
+		Author = "Universal",
+		Colour = Color3.fromRGB(141, 42, 22),
+		Code = "https://raw.githubusercontent.com/Glowing-Red/Script-Hub/main/Server%20Browser.lua",
+	},
+	CmdX = {
+		Name = "CMD-X",
+    SearchName = "CMD-X",
+		Author = "Universal",
+		Colour = Color3.fromRGB(141, 42, 22),
+		Code = "https://raw.githubusercontent.com/CMD-X/CMD-X/master/Source",
+	},
+	InfiniteYield = {
+		Name = "Infinite Yield",
+    SearchName = "Infinite Yield",
+		Author = "Universal",
+		Colour = Color3.fromRGB(141, 42, 22),
+		Code = "https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source",	
+	},
+	EzHub = {
+		Name = "EZHub",
+    SearchName = "EzHub",
+		Author = "Hub",
+		Colour = Color3.fromRGB(77, 102, 191),
+		Code = "https://raw.githubusercontent.com/debug420/Ez-Industries-Launcher-Data/master/Launcher.lua"
+	},
+}
+
+return ScriptsStorage
