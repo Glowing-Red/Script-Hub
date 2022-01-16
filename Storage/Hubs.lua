6f2874ef99557e3ab3258ac0885e132120d74eb6
Glowing-Red <79102132+Glowing-Red@users.noreply.github.com>
2022-01-16 13:32:30 +0100

    Update Hubs.lua

--- Storage/Hubs.lua
+++ Storage/Hubs.lua
@@ -20,16 +20,6 @@ Hubs = {
 		BackgroundColour = Color3.fromRGB(77, 102, 191),
 		Code = "https://raw.githubusercontent.com/debug420/Ez-Industries-Launcher-Data/master/Launcher.lua"
 	},
-	Testing = {
-		Name = "Testing",
-		SearchName = "Testing",
-		Icon = "Default",
-		CustomIcon = "None",
-		CustomIconColour = Color3.fromRGB(77, 102, 191),
-		CustomTextColour = Color3.fromRGB(255, 255, 255),
-		BackgroundColour = Color3.fromRGB(77, 102, 191),
-		Code = "https://raw.githubusercontent.com/debug420/Ez-Industries-Launcher-Data/master/Launcher.lua"
-	},
 }
 
 return Hubs
