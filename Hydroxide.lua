f90a169d5b2118cfae4a97ceb705fb8a096c5a41
Glowing-Red <79102132+Glowing-Red@users.noreply.github.com>
2022-02-05 12:49:35 +0100

    Delete Hydroxide.lua

--- Hydroxide.lua
+++
@@ -1,6 +0,0 @@
-local function Loadstring(owner, repositorie, branch, file)
-    return loadstring(game:HttpGetAsync(("https://raw.githubusercontent.com/%s/%s/%s/%s.lua"):format(owner, repositorie, branch, file)), file..'.lua')()
-end
-
-Loadstring('Upbolt', 'Hydroxide', 'revision', 'init')
-Loadstring('Upbolt', 'Hydroxide', 'revision', 'ui/main')
